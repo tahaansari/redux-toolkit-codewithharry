4bd4ac9234cdd25555a5b1f30ef384060396e3f4
Taha Ansari <taha.m.ansari@gmail.com>
2024-07-21 12:49:29 +0530

    wip

---
+++ src/Shop.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <>
+      <h1>ADIDAS SHOES RS 1000</h1>
+      <div className="text-center">
+        <div className="d-flex justify-content-center align-items-center">
+          <button className="btn btn-primary">-</button>
+          <p className="mb-0 mx-3">add this item to cart</p>
+          <button className="btn btn-primary">+</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Shop;
